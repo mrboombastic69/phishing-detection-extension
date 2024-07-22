e8697f209d0edaaa316de7c803506c0d39820470
Premansh-Pareek <premansh.pareek@symplora.com>
2024-07-22 21:26:37 +0530

    Created content.js for Firefox extension

---
+++ extension/src/content.js
@@ -0,0 +1,51 @@
+console.log('Content script is running');
+
+function getEmailContent() {
+  let emailContentElement = document.querySelector('.ii.gt');
+  if (emailContentElement) {
+    console.log('Email content extracted:', emailContentElement.innerText);
+    return emailContentElement.innerText;
+  }
+  console.log('No email content found.');
+  return '';
+}
+
+function sendEmailContent() {
+  const emailContent = getEmailContent();
+  if (emailContent && emailContent !== previousEmailContent) {
+    browser.runtime.sendMessage({ action: 'checkEmail', content: emailContent });
+    console.log('Sent email content to background script:', emailContent);
+    previousEmailContent = emailContent; // Update the previously sent content
+    emailContentSent = true; // Set flag to true after sending
+  }
+}
+
+// Debounce function to limit how often sendEmailContent is called
+function debounce(func, wait) {
+  let timeout;
+  return function() {
+    const context = this, args = arguments;
+    clearTimeout(timeout);
+    timeout = setTimeout(() => func.apply(context, args), wait);
+  };
+}
+
+// Create a debounced version of sendEmailContent
+const debouncedSendEmailContent = debounce(sendEmailContent, 2000);
+
+// MutationObserver to detect changes in the email content area
+const observer = new MutationObserver((mutations) => {
+  mutations.forEach((mutation) => {
+    if (mutation.addedNodes.length > 0) {
+      debouncedSendEmailContent();
+    }
+  });
+});
+
+// Observe changes in the Gmail email content area
+const emailContentArea = document.querySelector('body');
+if (emailContentArea) {
+  observer.observe(emailContentArea, { childList: true, subtree: true });
+} else {
+  console.log('Email content area not found.');
+}
